552d0d6d1eea74b59c94715c009a43ec796a1e80
amadeuszpolak <amadeusz.polak@gmail.com>
2015-01-26 11:15:39 +0100

    Initial Commit

---
+++ popUpMenuAfterSuccessOrFailureOrPauseInLevel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  popUpMenuAfterSuccessOrFailureOrPauseInLevel
+//
+//  Created by Macbook on 26.01.2015.
+//  Copyright (c) 2015 Amadeusz Polak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
